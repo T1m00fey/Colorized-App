6b5092c26446c48c1b2327c9b41a0d85512b1322
T1m00fey <86416015+Tim123456Tim@users.noreply.github.com>
2023-06-13 18:22:08 +0300

    Without TF

--- Colorized App/ContentView.swift
+++ Colorized App/ContentView.swift
@@ -8,19 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var redSliderValue = Double.random(in: 0...255)
+    @State private var greenSliderValue = Double.random(in: 0...255)
+    @State private var blueSliderValue = Double.random(in: 0...255)
+    
+    @State var redColor = Color.red
+    @State var greenColor = Color.green
+    @State var blueColor = Color.blue
+    
+    var color: Color {
+        Color(
+            red: redSliderValue / 255,
+            green: greenSliderValue /  255,
+            blue: blueSliderValue / 255
+        )
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(red: 0, green: 0.4, blue: 0.6)
+                .ignoresSafeArea()
+            VStack {
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: 300, height: 150)
+                    .foregroundColor(color)
+                    .padding(.top, 50)
+                    .padding(.leading, 16)
+                Spacer()
+                SliderView(value: $redSliderValue, color: $redColor)
+                SliderView(value: $greenSliderValue, color: $greenColor)
+                SliderView(value: $blueSliderValue, color: $blueColor)
+                    .padding(.bottom, 300)
+            }
+            .padding(.trailing, 16)
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct SliderView: View {
+    @Binding var value: Double
+    @Binding var color: Color
+    
+    var body: some View {
+        HStack {
+            Text("\(lround(value))")
+                .frame(width: 40, height: 20)
+                .foregroundColor(.white)
+            Slider(value: $value, in: 0...255, step: 1)
+                .accentColor(color)
+        }
     }
 }
