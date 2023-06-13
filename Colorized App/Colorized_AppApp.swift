01af80d4b0f1988bad134cb52576d5d2410fd0dc
Tim123456Tim <86416015+Tim123456Tim@users.noreply.github.com>
2023-06-13 17:31:01 +0400

    Initial Commit

---
+++ Colorized App/Colorized_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Colorized_AppApp.swift
+//  Colorized App
+//
+//  Created by Тимофей Юдин on 13.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Colorized_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
